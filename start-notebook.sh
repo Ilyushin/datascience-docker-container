45092b14f4ee4bdf2167361903db5927a88262d9
Eugene Ilyushin <john.ilyushin@gmail.com>
2018-09-02 08:18:22 +0300

    Fixed tensorboard

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd /home
+tensorboard --logdir=./tensorboard_logs --port=7007
 exec jupyter notebook --NotebookApp.token='' --ip=0.0.0.0 --allow-root --port=8888
-exec tensorboard --logdir=./tensorboard_logs --port=7007
